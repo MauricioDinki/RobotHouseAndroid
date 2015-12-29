f164e6dd3e83ad06ac97ecd6cfcc8f523ad454c6
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2015-12-29 14:12:41 -0600

    Add splash screen :rocket:

---
+++ app/src/main/java/com/mauriciodinki/robothouse/ui/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.mauriciodinki.robothouse.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mauriciodinki.robothouse.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(1000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    /*Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);*/
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
